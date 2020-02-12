d6815543ead4c0980c764ab37b245103bd0b4076
Ian Melvin <ian.melvin@mymail.champlain.edu>
2020-02-11 19:55:53 -0500

    Confirmed Launcher works

--- finalBoss_Dan/Launch_VS.bat
+++ finalBoss_Dan/Launch_VS.bat
@@ -1,3 +1,3 @@
 @echo off
 set GPRO_FW=%~dp0
-"%GPRO_FW%project\VisualStudio\_SLN\finalBoss_Dan\finalBoss_Dan.sln"
+"%GPRO_FW%project\VisualStudio\finalBoss_Dan\_SLN\finalBoss_Dan\finalBoss_Dan.sln"
