84005f49f83c7c8bf143b418c5575310892f755b
Loki Sanguine <nicholas.sanguine@CHAMPLAIN.EDU>
2020-02-11 19:59:28 -0500

    filled with function shells

--- finalBoss_Dan/source/finalBossDantest/main.cpp
+++ finalBoss_Dan/source/finalBossDantest/main.cpp
@@ -1,6 +1,32 @@
-
-
 int main()
 {
-	
+
 }
+
+//finished Part 2 Step 3, on Step 4
+int gameState()
+{
+	//pass W L T by reference
+}
+
+void endGame();//if return was W T or L switch case print
+
+
+int drawGame(); //draw our game
+
+int playerXY() //determine who is who
+{};
+/*
+//Header data
+#pragma once
+int gameState()
+{
+
+}
+
+void endGame();
+
+int drawGame();
+
+int playerXY();
+*/
